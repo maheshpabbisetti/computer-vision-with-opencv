e2cb21350bda8ee0db96a82e3ab5d99b7adee6bf
maheshpabbisetti <113224451+maheshpabbisetti@users.noreply.github.com>
2023-05-02 13:54:34 +0530

    Create rotate image(180).py

---
+++ rotate image(180).py
@@ -0,0 +1,8 @@
+#image rotation
+import cv2
+path = "C:/Users/maheshpabbisetti/OneDrive/Pictures/Camera Roll/WIN_20220923_11_27_50_Pro2.jpg"
+src = cv2.imread(path)
+window_name = 'Image'
+image = cv2.rotate(src, cv2.ROTATE_180)
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
